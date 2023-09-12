297ad7ac9bc5d579b78c82ae3e52c8495a806f8a
Samuel Kerstell <samuel.kerstell@outlook.com>
2023-09-12 22:51:25 +0000

    deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,11 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    '8000-samuelkerste-djangoblog-shz8lkdonic.ws-eu104.gitpod.io',
-    'https://django-blog-sam-fce15e3c062d.herokuapp.com',
-    'localhost',
-]
+ALLOWED_HOSTS = ['django-blog-sam.herokuapp.com', 'localhost',]
 
 
 # Application definition
